47821b60973fd70980c929bf3fe12b2592af7808
Emperor <phil@grulo.com>
2019-10-04 15:16:26 -0500

    less terribad code

---
+++ README.md
@@ -0,0 +1,63 @@
+# About Hatter.
+
+![Mad Hatter Picture](https://i.imgur.com/ckiRMPN.jpg)
+
+This is **mad as a Hatter**! It's used for [Empires and Puzzles](https://www.smallgiantgames.com/empires-and-puzzles) game to gather raid data from screenshots. 
+
+I have used this data to help reach #1 in the world several times in a row.
+
+# Is this cheating?
+
+- I don't believe  so. You're simply collecting data on what your top colors are so you can maximize your chances of winning through simple statistics. 
+- To prove the point, I reached #1 globally multiple times with the least likely color combinations, meaning the deck was stacked against me the whole way. You still need to know how to play, how to counteract, and you absolutely need good heroes. 
+
+# Installation:
+
+```
+git clone https://github.com/MarkBuffalo/hatter.git
+cd hatter
+pip3 install -r requirements.txt
+mkdir shots
+```
+
+# Additional program requirements
+
+- You must be using an iPhone X, or an emulator which allows you to take screenshots and set your resolution to the iPhone X's standard resolution, or modify the script to handle your resolution.
+- For this to work, you must put all pre-fight raid screenshots in the `/shots/` directory. 
+- Pre-fight means you take a screenshot of the first screen after the raid starts, and before anyone fights. You must not move any tiles.
+- You will need to build up thousands of screenshots to get any meaningful data.
+
+An example full-sized anonymous screenshot is placed below:
+
+![Empires and Puzzles: First-screen Raid Tiles](https://i.imgur.com/GYU1gxh.jpg)
+
+Example `/shots/` directory structure:
+
+```
+$ pwd
+/Users/derp/projects/hatter/shots
+$ ls -la 
+-rwxr-xr-x@  1 mad as  a hatter   9868724 Feb 09 22:26 IMG_1255.png
+-rwxr-xr-x@  1 mad as  a hatter   9860067 Feb 09 22:26 IMG_1256.png
+-rwxr-xr-x@  1 mad as  a hatter   9651928 Feb 09 22:26 IMG_1257.png
+-rwxr-xr-x@  1 mad as  a hatter   9724532 Feb 09 22:26 IMG_1258.png
+-rwxr-xr-x@  1 mad as  a hatter   9861563 Feb 09 22:26 IMG_1259.png
+[...]
+```
+
+Once you have enough screenshots in the `/shots/` directory, you can run the python script:
+
+```
+python3 hatter.py
+```
+
+This will give you an output like so:
+
+![Output](https://i.imgur.com/XpSB55u.png)
+
+...which you're free to analyze using your own data science tomfoolery. Note that I'm aware the output is a dictionary. An earlier iteration of hatter used a list.
+
+# Warning
+
+- This currently only works with iPhone X resolution screenshots. You will need to modify the script for other resolutions.
+- If your resolution differs, you will also have to update the `increment_amount` variable with the amount of spacing between tiles.
---
+++ hatter.py
@@ -0,0 +1,153 @@
+import webcolors
+from PIL import Image
+from collections import Counter
+import os
+from os import listdir
+
+
+class MadAsAHatter:
+    def __init__(self):
+        # Currently only supports the iPhone X resolution. Tweak for your own settings.
+        # The game has 7 columns
+        self.columns = 7
+        # And 5 rows.
+        self.rows = 5
+
+        # The first tile's X coordinates.
+        self.start_x = 99
+        # And the first tile's Y coordinates.
+        self.start_y = 1094
+
+        # The position where the middle of the next icon begins.
+        self.increment_amount = 154
+
+        # Dictionary map of web colors to closest RGBYP value. e.g.: olivedrab = green.
+        self.color_names = {
+            "sandybrown": "yellow",
+            "burlywood": "yellow",
+            "royalblue": "blue",
+            "purple": "purple",  # Shut up! I'm too lazy to write an if statement.
+            "olivedrab": "green",
+            "darkolivegreen": "green",
+            "firebrick": "red",
+            "brown": "red"
+        }
+
+        # The current list of each individual screenshot.
+        self.current_list = []
+
+        # The total accumulated value list of all parsed images.
+        self.all_values = []
+
+    # For the iPhone X screenshots.
+    # Edit for your own (see start_x and start_y). Good luck because I didn't calculate
+    # the positions based on screenshot size).
+    def scan_images(self):
+        # Take the current directory and append "/shots/". Put all your screenshots in the "/shots/" directory.
+        # Yeah, I could just use argparse, but whatever.
+        current_directory = os.path.dirname(os.path.realpath(__file__)) + "/shots/"
+        print(current_directory)
+        file_list = listdir(current_directory)
+
+        for file in file_list:
+            # Reset the current list each time so it doesn't fill up.
+            global current_list
+            current_list = []
+            img = Image.open(current_directory + file)
+            pixel_map = img.load()
+            # Now let's traverse the grid to see what data we can glean from it.
+            self.traverse_grid(pixel_map)
+        # And then print the final results.
+        print("\nTotal value collection:")
+        print(self.compute_current_average(self.all_values))
+
+    # Find tile data within the grid.
+    def traverse_grid(self, pixel_map):
+        cur_y = self.start_y
+
+        for row in range(self.rows):
+            print(f"Current row: {row+1}")
+            self.traverse_columns(cur_y, row, pixel_map)
+            cur_y += self.increment_amount
+        # Print out the average values in dictionary format.
+        self.compute_current_average(current_list)
+
+    def traverse_columns(self, cur_y, row, pixel_map):
+        # Now we're going to traverse horizontally.
+        cur_x = self.start_x
+
+        for i in range(self.columns):
+            # Convert the tuple to a list so we can edit it.
+            tile_rgb_list = list(pixel_map[cur_x, cur_y])
+            # We're going to get rid of the 'a' value in the rgba result by only combining RED[0], GREEN[1] and BLUE[2].
+            current_tile = (tile_rgb_list[0], tile_rgb_list[1], tile_rgb_list[2])
+            color_name = self.closest_color(current_tile)
+
+            print(f"Row: {row+1}. Column: {i+1}. Position: [{cur_x},{cur_y}]. "
+                  f"Color (rgba): {tile_rgb_list}. "
+                  f"Closest RGB color name: {color_name}")
+
+            # Store rol, col and color name for later parse.
+            # Update current list. It will get reset later.
+            current_list.append([row, i, color_name])
+            # Update total list. It will not be reset.
+            self.all_values.append([row, i, color_name])
+            # Increment the current X coordinate by however much we determined earlier.
+            cur_x += self.increment_amount
+
+    # We get our average this way...
+    def compute_current_average(self, values):
+        new_list = []
+        for i in values:
+            new_list.append(i[2])
+
+        sorted_order = self.get_sorted_list(new_list)
+        for key, val in sorted_order.items():
+            if key is not None and val is not None:
+                print(f"{key.upper()}: {val} occurrences.")
+
+    # This is our sorted dictionary. Get average values for each tile on each image parsed.
+    @staticmethod
+    def get_sorted_list(counter_list):
+        counted_list = Counter(counter_list)
+        # Sort list by highest occurrence to smallest occurrence.
+        # e.g.: {'green': 10, 'purple': 8, 'yellow': 8, 'red': 7, 'blue': 2}
+        return dict(sorted(counted_list.items(), key=lambda x: x[1], reverse=True))
+
+    # Get nearest web color based on euclidean distance
+    def closest_color(self, requested_color):
+        min_colors = {}
+        for key, name in webcolors.css3_hex_to_names.items():
+            r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+            rd = (r_c - requested_color[0]) ** 2
+            gd = (g_c - requested_color[1]) ** 2
+            bd = (b_c - requested_color[2]) ** 2
+            min_colors[(rd + gd + bd)] = name
+        # Now we're going to convert the nearest web color to the nearest RGBYP value.
+        # e.g.: red, green, blue, yellow, purple.
+        return self.closest_rgb(min_colors[min(min_colors.keys())])
+
+    # Map our current color name to something readable by a 4 year old.
+    def closest_rgb(self, color):
+        for k, v in self.color_names.items():
+            if color is k:
+                return v
+        # It's not mapped. Return color so we can fix it.
+        return color
+
+    # Get closest web color name
+    def get_color_name(self, requested_color):
+        try:
+            closest_name = actual_name = webcolors.rgb_to_name(requested_color)
+        # An exception was thrown, meaning we couldn't find the right color.
+        except ValueError:
+            # We found an error, so let's conjure euclidean horrors.
+            closest_name = self.closest_color(requested_color)
+            actual_name = None
+        return actual_name, closest_name
+
+
+if __name__ == "__main__":
+    mad = MadAsAHatter()
+    mad.scan_images()
+
