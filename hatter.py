15330ca6287c67f2ccc32337e17be3d1ec20c163
Emperor <phil@grulo.com>
2019-10-07 20:49:12 -0500

    resolution agnostic code

--- hatter.py
+++ hatter.py
@@ -14,12 +14,28 @@ class MadAsAHatter:
         self.rows = 5
 
         # The first tile's X coordinates.
-        self.start_x = 99
+        # This is based on the iPhone X screenshot length. Everything else is subject to calculation.
+        self.base_width = 1125
         # And the first tile's Y coordinates.
-        self.start_y = 1094
+        self.base_height = 2436
+        # This is based on the iPhone X resolution. Everything else is subject to calculation.
+        self.base_x = 99
+        self.base_y = 1094
 
-        # The position where the middle of the next icon begins.
-        self.increment_amount = 154
+        # Where the middle of the next icon starts. For iPhone X. Everything else is subject to calculation.
+        self.base_increment_amount = 154
+
+        self.current_width = 0
+        self.current_height = 0
+
+        # Get our base distance calculations so we can update based on new resolutions.
+        self.base_distance_x = self.base_x / self.base_width
+        self.base_distance_y = self.base_y / self.base_height
+
+        # We would update these values later.
+        self.new_distance_x = 0
+        self.new_distance_y = 0
+        self.new_increment_amount = 0
 
         # Dictionary map of web colors to closest RGBYP value. e.g.: olivedrab = green.
         self.color_names = {
@@ -30,7 +46,15 @@ class MadAsAHatter:
             "olivedrab": "green",
             "darkolivegreen": "green",
             "firebrick": "red",
-            "brown": "red"
+            "brown": "red",
+            "darkred": "red",
+            "forestgreen": "green",
+            "goldenrod": "yellow",
+            "indigo": "blue",
+            "darkgreen": "green",
+            "dodgerblue": "blue",
+            "darkred": "red",
+            "darkgoldenrod": "yellow"
         }
 
         # The current list of each individual screenshot.
@@ -39,10 +63,18 @@ class MadAsAHatter:
         # The total accumulated value list of all parsed images.
         self.all_values = []
 
+    # Called once per image, at the beginning, before any processing.
+    def reset_image_coordinates(self):
+        self.new_distance_x = int(round(self.current_width * self.base_distance_x))
+        self.new_distance_y = int(round(self.current_height * self.base_distance_y))
+        self.new_increment_amount = int(round(self.current_width * (self.base_increment_amount / self.base_width)))
+
     # For the iPhone X screenshots.
     # Edit for your own (see start_x and start_y). Good luck because I didn't calculate
     # the positions based on screenshot size).
     def scan_images(self):
+        print(self.base_distance_x)
+        print(self.base_distance_y)
         # Take the current directory and append "/shots/". Put all your screenshots in the "/shots/" directory.
         # Yeah, I could just use argparse, but whatever.
         current_directory = os.path.dirname(os.path.realpath(__file__)) + "/shots/"
@@ -50,10 +82,15 @@ class MadAsAHatter:
         file_list = listdir(current_directory)
 
         for file in file_list:
-            # Reset the current list each time so it doesn't fill up.
-            self.current_list = []
             img = Image.open(current_directory + file)
             pixel_map = img.load()
+
+            # Now, set the size of the screenshot so we can perform insane calculations later.
+            self.current_width, self.current_height = img.size
+            # Reset the coordinates each time we load a new image.
+            self.reset_image_coordinates()
+            # Reset the current list each time so it doesn't fill up.
+            self.current_list = []
             # Now let's traverse the grid to see what data we can glean from it.
             self.traverse_grid(pixel_map)
         # And then print the final results.
@@ -62,18 +99,18 @@ class MadAsAHatter:
 
     # Find tile data within the grid.
     def traverse_grid(self, pixel_map):
-        cur_y = self.start_y
+        cur_y = self.new_distance_y
 
         for row in range(self.rows):
             print(f"Current row: {row+1}")
             self.traverse_columns(cur_y, row, pixel_map)
-            cur_y += self.increment_amount
+            cur_y += self.new_increment_amount
         # Print out the average values in dictionary format.
         self.compute_current_average(self.current_list)
 
     def traverse_columns(self, cur_y, row, pixel_map):
         # Now we're going to traverse horizontally.
-        cur_x = self.start_x
+        cur_x = self.new_distance_x
 
         for i in range(self.columns):
             # Convert the tuple to a list so we can edit it.
@@ -92,7 +129,7 @@ class MadAsAHatter:
             # Update total list. It will not be reset.
             self.all_values.append([row, i, color_name])
             # Increment the current X coordinate by however much we determined earlier.
-            cur_x += self.increment_amount
+            cur_x += self.new_increment_amount
 
     # We get our average this way...
     def compute_current_average(self, values):
